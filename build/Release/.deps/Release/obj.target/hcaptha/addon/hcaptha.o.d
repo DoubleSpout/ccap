6f233cba05d3ca058fb3c5b6ce66da8e8acf80c8
Guo Yu <i@guoyu.me>
2014-09-20 17:46:59 +0800

    更新 dist files

--- build/Release/.deps/Release/obj.target/hcaptha/addon/hcaptha.o.d
+++ build/Release/.deps/Release/obj.target/hcaptha/addon/hcaptha.o.d
@@ -1,21 +1,14 @@
-cmd_Release/obj.target/hcaptha/addon/hcaptha.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/xudongichi/.node-gyp/0.10.18/src -I/Users/xudongichi/.node-gyp/0.10.18/deps/uv/include -I/Users/xudongichi/.node-gyp/0.10.18/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/hcaptha/addon/hcaptha.o.d.raw  -c -o Release/obj.target/hcaptha/addon/hcaptha.o ../addon/hcaptha.cc
+cmd_Release/obj.target/hcaptha/addon/hcaptha.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/Turing/.node-gyp/0.11.13/src -I/Users/Turing/.node-gyp/0.11.13/deps/uv/include -I/Users/Turing/.node-gyp/0.11.13/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/hcaptha/addon/hcaptha.o.d.raw  -c -o Release/obj.target/hcaptha/addon/hcaptha.o ../addon/hcaptha.cc
 Release/obj.target/hcaptha/addon/hcaptha.o: ../addon/hcaptha.cc \
-  /Users/xudongichi/.node-gyp/0.10.18/src/node.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-unix.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/ngx-queue.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-darwin.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/v8/include/v8.h \
-  /Users/xudongichi/.node-gyp/0.10.18/deps/v8/include/v8stdint.h \
-  /Users/xudongichi/.node-gyp/0.10.18/src/node_object_wrap.h \
-  ../addon/cap.h
+  /Users/Turing/.node-gyp/0.11.13/src/node.h \
+  /Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8.h \
+  /Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8stdint.h \
+  /Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8config.h \
+  /Users/Turing/.node-gyp/0.11.13/src/node_version.h ../addon/cap.h
 ../addon/hcaptha.cc:
-/Users/xudongichi/.node-gyp/0.10.18/src/node.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-unix.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/ngx-queue.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-darwin.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/v8/include/v8.h:
-/Users/xudongichi/.node-gyp/0.10.18/deps/v8/include/v8stdint.h:
-/Users/xudongichi/.node-gyp/0.10.18/src/node_object_wrap.h:
+/Users/Turing/.node-gyp/0.11.13/src/node.h:
+/Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8.h:
+/Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8stdint.h:
+/Users/Turing/.node-gyp/0.11.13/deps/v8/include/v8config.h:
+/Users/Turing/.node-gyp/0.11.13/src/node_version.h:
 ../addon/cap.h:
